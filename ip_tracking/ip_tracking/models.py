81c9d6fd7cce4afb64bc857951e3a3d10540753e
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-11-05 16:09:36 +0100

    Update models.py

--- ip_tracking/ip_tracking/models.py
+++ ip_tracking/ip_tracking/models.py
@@ -8,3 +8,20 @@ class RequestLog(models.Model):
     def __str__(self):
         return f"{self.ip_address} - {self.path} - {self.timestamp}"
 
+
+from django.db import models
+
+class RequestLog(models.Model):
+    ip_address = models.GenericIPAddressField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    path = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.path} - {self.timestamp}"
+
+
+class BlockedIP(models.Model):
+    ip_address = models.GenericIPAddressField(unique=True)
+
+    def __str__(self):
+        return self.ip_address
