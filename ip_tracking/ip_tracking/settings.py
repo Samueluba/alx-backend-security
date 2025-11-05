4989bd8121b382a62f5bd2f611ad44a6c764c2da
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-11-05 16:16:56 +0100

    Update settings.py

--- ip_tracking/ip_tracking/settings.py
+++ ip_tracking/ip_tracking/settings.py
@@ -1,1 +1,9 @@
+from celery.schedules import crontab
+
+CELERY_BEAT_SCHEDULE = {
+    'detect-suspicious-ips-hourly': {
+        'task': 'ip_tracking.tasks.detect_suspicious_ips',
+        'schedule': crontab(minute=0, hour='*'),  # every hour
+    },
+}
 
